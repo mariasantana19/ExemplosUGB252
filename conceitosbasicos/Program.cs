4083acfba3324d17f228ba8aedafc97ade654fd0
2024101382 <2024101382@LAB1-016>
2025-08-12 19:23:11 -0300

    Adicionar arquivos de projeto.

---
+++ conceitosbasicos/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more informatio
+int idade = 24;
+Console.WriteLine("a idade é" + idade);
+if (idade >= 18)
+{
+    Console.WriteLine("voce pode tirar habilitaçao");
+}
