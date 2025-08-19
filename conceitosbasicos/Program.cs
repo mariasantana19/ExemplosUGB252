2de8eb83f220b54df858dfd38b75faa8bff5947b
mariasantana19 <santanamaria15555@gmail.com>
2025-08-19 19:35:27 -0300

    calculo irpf desenvolvido

--- conceitosbasicos/Program.cs
+++ conceitosbasicos/Program.cs
@@ -1,9 +1,29 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿
+using System;
 
 Console.WriteLine( "informe seu nome");
 string nome = Console.ReadLine();
 Console.WriteLine("informe seu salario");
-decimal salarioSrt = decimal.Parse(Console.ReadLine());
+decimal salario = decimal.Parse(Console.ReadLine());
 
-Console.WriteLine($"meu nome é" {nome} "meu salario é"{salario} );
 Console.WriteLine("seja bem-vinda(o) {0}, o salario informado foi {1}", nome, salario);
+
+//informem o valor do IRPF  a ser pago de acordo com o salario informado. Utiliza a tabela real de desconto de IRPF.
+
+decimal irpfDevido;
+ if (salario <= 2259.20m)
+    irpfDevido = 0;
+
+else if (salario <= 2826.65m)
+    irpfDevido = salario * 0.075m - 169.21m;
+
+else if (salario <= 3751.05m)
+    irpfDevido = salario * 0.15m - 381.44m;
+
+else if (salario <= 4664.68m)
+    irpfDevido = salario * 0.225m - 662.77m;
+
+else
+    irpfDevido = salario * 0.275m - 896m;
+
+Console.WriteLine($"para o salario de {salario} o valor a ser pago de IRPF é{irpfDevido}, entao o salario com desconto de IRPF sera {salario - irpfDevido}");
